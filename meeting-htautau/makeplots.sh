c65893bbc4ebd1da5a57244097d0630f5704dda2
Raphael Friese <raphael.friese@cern.ch>
2016-02-16 19:19:40 +0100

    savepoint

--- meeting-htautau/makeplots.sh
+++ meeting-htautau/makeplots.sh
@@ -1,3 +1,6 @@
+harry.py -j MVAMET_Response.json MVAMET_Response_pt.json MVAMET_template.json MVAMET_ptplot.json
+harry.py -j MVAMET_Response.json MVAMET_Response_NPV.json MVAMET_template.json MVAMET_npvplot.json
+
 harry.py -j MVAMET_template.json MVAMET_Transverse_Resolution_pt.json MVAMET_Transverse_Resolution.json MVAMET_resolutionplot.json MVAMET_ptplot.json
 harry.py -j MVAMET_template.json MVAMET_Transverse_Resolution_NPV.json MVAMET_Transverse_Resolution.json MVAMET_resolutionplot.json MVAMET_npvplot.json
 
