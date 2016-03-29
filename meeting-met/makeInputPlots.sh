303f70731e66edaec04078256465c7be5fc2a7f1
Raphael Friese <raphael.friese@cern.ch>
2016-03-29 20:02:11 +0200

    settings for plots

---
+++ meeting-met/makeInputPlots.sh
@@ -0,0 +1,7 @@
+harry.py -j template.json Longitudinal_Response_pt.json 
+harry.py -j template.json Longitudinal_Response_NPV.json  
+harry.py -j template.json resolutionplot.json Longitudinal_Resolution_pt.json  
+harry.py -j template.json resolutionplot.json Longitudinal_Resolution_NPV.json  
+harry.py -j template.json resolutionplot.json Transverse_Resolution_NPV.json 
+harry.py -j template.json resolutionplot.json Transverse_Resolution_pt.json 
+#harry.py -j template.json sumET.json 
---
+++ meeting-met/makeplots.sh
@@ -0,0 +1,8 @@
+harry.py -j MVAMET_Response.json MVAMET_Response_pt.json MVAMET_template.json MVAMET_ptplot.json
+harry.py -j MVAMET_Response.json MVAMET_Response_NPV.json MVAMET_template.json MVAMET_npvplot.json
+
+harry.py -j MVAMET_template.json MVAMET_Transverse_Resolution_pt.json MVAMET_Transverse_Resolution.json MVAMET_resolutionplot.json MVAMET_ptplot.json
+harry.py -j MVAMET_template.json MVAMET_Transverse_Resolution_NPV.json MVAMET_Transverse_Resolution.json MVAMET_resolutionplot.json MVAMET_npvplot.json
+
+harry.py -j MVAMET_template.json MVAMET_Longitudinal_Resolution_pt.json MVAMET_Longitudinal_Resolution.json MVAMET_resolutionplot.json MVAMET_ptplot.json
+harry.py -j MVAMET_template.json MVAMET_Longitudinal_Resolution_NPV.json MVAMET_Longitudinal_Resolution.json MVAMET_resolutionplot.json MVAMET_npvplot.json
