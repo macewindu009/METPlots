67a0c3101e4cd694fd9047a4d1756b21a914845d
Raphael Friese <raphael.friese@cern.ch>
2016-08-12 15:28:19 +0200

    MVA MET Input plots

--- thesis/makeplots.sh
+++ thesis/makeplots.sh
@@ -1,3 +1,4 @@
+# MVA MET plots
 harry.py -j MVAMET_Response.json MVAMET_Response_pt.json MVAMET_template.json MVAMET_ptplot.json
 harry.py -j MVAMET_Response.json MVAMET_Response_NPV.json MVAMET_template.json MVAMET_npvplot.json
 
@@ -6,3 +7,13 @@ harry.py -j MVAMET_template.json MVAMET_Transverse_Resolution_NPV.json MVAMET_Tr
 
 harry.py -j MVAMET_template.json MVAMET_Longitudinal_Resolution_pt.json MVAMET_Longitudinal_Resolution.json MVAMET_resolutionplot.json MVAMET_ptplot.json --legend 'lower right'
 harry.py -j MVAMET_template.json MVAMET_Longitudinal_Resolution_NPV.json MVAMET_Longitudinal_Resolution.json MVAMET_resolutionplot.json MVAMET_npvplot.json
+
+# input plots
+harry.py -j INPUT_Response.json INPUT_Response_pt.json INPUT_template.json MVAMET_ptplot.json
+harry.py -j INPUT_Response.json INPUT_Response_NPV.json INPUT_template.json MVAMET_npvplot.json
+
+harry.py -j INPUT_template.json INPUT_Transverse_Resolution_pt.json INPUT_Transverse_Resolution.json INPUT_resolutionplot.json MVAMET_ptplot.json
+harry.py -j INPUT_template.json INPUT_Transverse_Resolution_NPV.json INPUT_Transverse_Resolution.json INPUT_resolutionplot.json MVAMET_npvplot.json
+
+harry.py -j INPUT_template.json INPUT_Longitudinal_Resolution_pt.json INPUT_Longitudinal_Resolution.json INPUT_resolutionplot.json MVAMET_ptplot.json --legend 'lower right'
+harry.py -j INPUT_template.json INPUT_Longitudinal_Resolution_NPV.json INPUT_Longitudinal_Resolution.json INPUT_resolutionplot.json MVAMET_npvplot.json
