656fb05b9035253c92f6a6f81f2b464e375f70b1
Raphael Friese <raphael.friese@cern.ch>
2016-09-19 15:38:34 +0200

    mm control plots and measurements

---
+++ thesis-data/mm2016-performance/makeplots.sh
@@ -0,0 +1,8 @@
+# MVA MET plots
+
+harry.py -j MVAMET_template.json MVAMET_Transverse_Resolution_pt.json MVAMET_Transverse_Resolution.json MVAMET_resolutionplot.json MVAMET_ptplot.json
+harry.py -j MVAMET_template.json MVAMET_Transverse_Resolution_NPV.json MVAMET_Transverse_Resolution.json MVAMET_resolutionplot.json MVAMET_npvplot.json
+
+harry.py -j MVAMET_template.json MVAMET_Longitudinal_Resolution_pt.json MVAMET_Longitudinal_Resolution.json MVAMET_resolutionplot.json MVAMET_ptplot.json --legend 'lower right'
+harry.py -j MVAMET_template.json MVAMET_Longitudinal_Resolution_NPV.json MVAMET_Longitudinal_Resolution.json MVAMET_resolutionplot.json MVAMET_npvplot.json
+
